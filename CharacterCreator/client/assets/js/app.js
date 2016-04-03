58ccff808a31e4d8e3cbd0c553fb8013ca0f3c1d
Manuel Schermuly <prinzeru@gmail.com>
2016-04-04 01:41:16 +0200

    i

--- CharacterCreator/client/assets/js/app.js
+++ CharacterCreator/client/assets/js/app.js
@@ -1,34 +1,53 @@
-(function() {
-  'use strict';
+(function () {
+	'use strict';
 
-  angular.module('application', [
-    'ui.router',
-    'ngAnimate',
+	var appModule = angular.module('application', [
+		'ui.router',
+		'ngAnimate',
 
-    //foundation
-    'foundation',
-    'foundation.dynamicRouting',
-    'foundation.dynamicRouting.animations'
-  ])
-    .config(config)
-    .run(run)
-  ;
+		//foundation
+		'foundation',
+		'foundation.dynamicRouting',
+		'foundation.dynamicRouting.animations'
+	])
+		.config(config)
+		.run(run)
+	;
 
-  config.$inject = ['$urlRouterProvider', '$locationProvider'];
+	config.$inject = ['$urlRouterProvider', '$locationProvider'];
 
-  function config($urlProvider, $locationProvider) {
-    $urlProvider.otherwise('/');
+	function config($urlProvider, $locationProvider) {
+		$urlProvider.otherwise('/');
 
-    $locationProvider.html5Mode({
-      enabled:false,
-      requireBase: false
-    });
+		$locationProvider.html5Mode({
+			enabled: false,
+			requireBase: false
+		});
 
-    $locationProvider.hashPrefix('!');
-  }
+		$locationProvider.hashPrefix('!');
+	}
 
-  function run() {
-    FastClick.attach(document.body);
-  }
+	function run() {
+		FastClick.attach(document.body);
+	}
 
+	appModule.controller('CharacterController', ['$q', '$scope', '$http', function ($q, $scope, $http, $location) {
+
+		$http.get('./assets/js/data.json')
+			.then(function(res){
+				$scope.data = res.data;
+				console.log(res.data)
+			});
+
+		console.log(JSON.stringify($scope.data));
+	}]);
+
+	appModule.directive('sheet', function () {
+		return {
+			restrict: 'A',
+			templateUrl: 'templates/directives/sheet.html',
+			// todo: shouldn't need to inherit entire scope
+			scope: true
+		};
+	});
 })();
