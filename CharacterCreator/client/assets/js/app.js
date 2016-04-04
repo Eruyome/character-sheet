bb233275050b8d30c3a27b024ee5f36b1c5e93ce
Manuel Schermuly <prinzeru@gmail.com>
2016-04-05 00:39:43 +0200

    update

--- CharacterCreator/client/assets/js/app.js
+++ CharacterCreator/client/assets/js/app.js
@@ -31,23 +31,138 @@
 		FastClick.attach(document.body);
 	}
 
-	appModule.controller('CharacterController', ['$q', '$scope', '$http', function ($q, $scope, $http, $location) {
+	appModule.controller('characterController', ['$scope', '$http', '$timeout', function ($scope, $http, $timeout) {
+		$scope.data = {};
+		$scope.showUnskilled = true;
+		$scope.showValueBase = true;
+		$scope.languageIndex = 0;
+		$scope.currentTab = 0;
+		$scope.player = {
+			"stats_base" : {
+				"dex" : { "name" : ["DEX", "GE"], "value" : 10 },
+				"edu" : { "name" : ["EDU", "BI"], "value" : 15 }
+			}
+		};
 
-		$http.get('./assets/js/data.json')
+		/* Load JSON */
+		$http.get('./assets/json/data.json')
 			.then(function(res){
 				$scope.data = res.data;
-				console.log(res.data)
+				console.log($scope.data.abilities);
 			});
 
-		console.log(JSON.stringify($scope.data));
+		/* Tabs */
+		$scope.onClickTab = function (tabId) {
+			$scope.currentTab = tabId;
+		};
+		$scope.isActiveTab = function(tabId) {
+			return tabId == $scope.currentTab;
+		};
+
+		/* Ability  Lists*/
+		$scope.getLength = function(data){
+			try {
+				return Math.floor(data.abilities.common.length/2)
+			}
+			catch(err){
+				console.info('Data not ready yet.')
+			}
+		};
+
+		var operators = {
+			'+': function(a, b){ return a+b},
+			'*': function(a, b){ return a*b},
+			'/': function(a, b){ return a/b},
+			'-': function(a, b){ return a-b}
+		};
+
+		function calculateAbilityValues(obj) {
+			Object.keys(obj).forEach(function(key,index) {
+				var length = obj[key].length;
+				var stat, operator, modifier, currentStatValue;
+
+				for (var i = 0; i <= length; i++) {
+					try {
+						if (typeof obj[key][i].calc !== "undefined") {
+							stat = obj[key][i].calc.stat;
+							operator = obj[key][i].calc.operator;
+							modifier = obj[key][i].calc.modifier;
+							currentStatValue = $scope.data.player.stats_base[stat].value;
+
+							obj[key][i].value_calc = operators[operator](currentStatValue, modifier);
+						}
+					}
+					catch (err){}
+				}
+			});
+		}
+
+		$scope.calculateDependencies = function(key, stat){
+
+			// calcualte ability base values
+			calculateAbilityValues($scope.data.abilities);
+
+			// calculate dmg bonus
+			if(key == 'str' || key == 'siz') {
+				var v = $scope.data.player.stats_base.str.value + $scope.data.player.stats_base.siz.value;
+				var result = 0;
+
+				if (v >= 2 && v <= 12 ) { result = '-1D6' }
+				else if (v >= 13 && v <= 16 ) { result = '-1D4' }
+				else if (v >= 17 && v <= 24 ) { result = '+0' }
+				else if (v >= 25 && v <= 32 ) { result = '+1D4' }
+				else if (v >= 33) { result = '+1D6' }
+
+				$scope.data.player.stats_calc.dmg_bonus.value = result;
+			}
+
+			if (key == 'pow') {
+				var v = (stat.value * 5) < 99 ? (stat.value * 5) : 99;
+				$scope.data.player.stats_calc.san.value = v;
+				v = (stat.value * 5) < 99 ? (stat.value * 5) : 99;
+				$scope.data.player.stats_calc.luck.value = v;
+			}
+			if (key == 'int') {
+				var v = (stat.value * 5) < 99 ? (stat.value * 5) : 99;
+				$scope.data.player.stats_calc.idea.value = v;
+			}
+			if (key == 'edu') {
+				var v = (stat.value * 5) < 99 ? (stat.value * 5) : 99;
+				$scope.data.player.stats_calc.know.value = v;
+			}
+		};
+
+
+		/* Helper */
+		$scope.isUndefined = function(e){
+			return typeof e === 'undefined';
+		};
 	}]);
+
+	/* Directives */
 
 	appModule.directive('sheet', function () {
 		return {
 			restrict: 'A',
 			templateUrl: 'templates/directives/sheet.html',
-			// todo: shouldn't need to inherit entire scope
 			scope: true
 		};
 	});
+
+	appModule.directive('ability', function () {
+		return {
+			restrict: 'A',
+			templateUrl: 'templates/directives/ability.html',
+			scope: true,
+			replace: true
+		};
+	});
+	appModule.directive('ability2', function () {
+		return {
+			restrict: 'A',
+			templateUrl: 'templates/directives/ability2.html',
+			scope: true,
+			replace: true
+		};
+	})
 })();
